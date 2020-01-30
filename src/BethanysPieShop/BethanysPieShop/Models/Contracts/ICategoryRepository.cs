17281b7015670c7e98b713e3ec76c6768e351d99
Tsonko Stoyanov <tsonko_stoyanov@stemo.bg>
2020-01-30 10:11:25 +0200

    Add models

---
+++ src/BethanysPieShop/BethanysPieShop/Models/Contracts/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace BethanysPieShop.Models.Contracts
+{
+    public interface ICategoryRepository
+    {
+        IEnumerable<Category> AllCategories { get; }
+    }
+}
---
+++ src/BethanysPieShop/BethanysPieShop/Models/MockCategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using BethanysPieShop.Models.Contracts;
+using System.Collections.Generic;
+
+namespace BethanysPieShop.Models
+{
+
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> AllCategories =>
+            new List<Category>
+            {
+                new Category{Id=1, Name="Fruit pies", Description="All-fruity pies"},
+                new Category{Id=2, Name="Cheese cakes", Description="Cheesy all the way"},
+                new Category{Id=3, Name="Seasonal pies", Description="Get in the mood for a seasonal pie"}
+            };        
+    }
+
+}
